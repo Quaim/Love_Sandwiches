f569cd19151fce186b0b2516311869af9df735e3
Ellis Taylor <ellis.taylor@live.com>
2024-02-19 15:56:47 +0000

    Add validation to check if entered data is correct number of values and emmutable to integers

--- run.py
+++ run.py
@@ -24,7 +24,23 @@ def get_sales_data():
     print("Example: 10, 20, 30, 40, 50, 60\n")
 
     data_str=input("Enter your data here: ")
-    print(f"the data provided is {data_str}")
+    
+    sales_data = data_str.split(",")
+    
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values to integers, provides an error feedback if that is not possible and if the number of values isnt exactly 6
+
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again")
 
 get_sales_data()
 
