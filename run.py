388c9b89f3bc7197aca13eb434398b3380140038
Ellis Taylor <ellis.taylor@live.com>
2024-02-19 17:46:20 +0000

    added function to update surplus worksheet with result from the surplus calculation function

--- run.py
+++ run.py
@@ -83,6 +83,13 @@ def calculate_surplus_data(sales_row):
         surplus_data.append(surplus)
     return(surplus_data)
 
+def update_surplus_worksheet(new_surplus_data):
+    """
+    Updates surplus worksheet adding new row with the latest surplus data
+    """
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(new_surplus_data)
+    print("Surplus worksheet updated succesfuly\n")
 
 
 
@@ -94,6 +101,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
